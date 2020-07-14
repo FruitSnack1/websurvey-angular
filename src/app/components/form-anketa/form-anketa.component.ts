933c46c04f65793182a43fe8c7bbbb04be11b593
fruitsnack1 <tomas.ryzner@email.cz>
2020-07-14 12:39:12 +0200

    open questions

--- src/app/components/form-anketa/form-anketa.component.ts
+++ src/app/components/form-anketa/form-anketa.component.ts
@@ -1,71 +1,75 @@
-import { Component, OnInit, NgModule } from '@angular/core'
-import { FormGroup, FormBuilder, FormArray } from '@angular/forms'
-import { AnketyService } from 'src/app/services/ankety.service'
-import { Router } from '@angular/router'
-
+import { Component, OnInit, NgModule } from "@angular/core";
+import { FormGroup, FormBuilder, FormArray } from "@angular/forms";
+import { AnketyService } from "src/app/services/ankety.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-form-anketa',
-  templateUrl: './form-anketa.component.html',
-  styleUrls: ['./form-anketa.component.css']
+  selector: "app-form-anketa",
+  templateUrl: "./form-anketa.component.html",
+  styleUrls: ["./form-anketa.component.css"],
 })
-
-
 export class FormAnketaComponent implements OnInit {
+  anketaForm: FormGroup;
 
-  anketaForm: FormGroup
-
-  constructor(private fb: FormBuilder, private anketyService: AnketyService, private router:Router) { }
+  constructor(
+    private fb: FormBuilder,
+    private anketyService: AnketyService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    
     this.anketaForm = this.fb.group({
-      name: '',
-      description: '',
+      name: "",
+      description: "",
       random_order: false,
       user_data: false,
       answers: this.fb.array([]),
-      questions: this.fb.array([])
-    })
+      questions: this.fb.array([]),
+    });
 
-    const defaultAnswers = ['Určitě ano', 'Spíše ano', 'Nevím', 'Spíše ne', 'Určitě ne']
+    const defaultAnswers = [
+      "Určitě ano",
+      "Spíše ano",
+      "Nevím",
+      "Spíše ne",
+      "Určitě ne",
+    ];
     for (let i = 0; i < defaultAnswers.length; i++) {
       const obj = this.fb.group({
         name: [defaultAnswers[i]],
-        value: [i+1]
-      })
-      this.answerForms.push(obj)
+        value: [i + 1],
+      });
+      this.answerForms.push(obj);
     }
 
-    this.addQuestion()
+    this.addQuestion();
   }
 
   get questionForms() {
-    return this.anketaForm.get('questions') as FormArray
+    return this.anketaForm.get("questions") as FormArray;
   }
 
   get answerForms() {
-    return this.anketaForm.get('answers') as FormArray
+    return this.anketaForm.get("answers") as FormArray;
   }
 
   addQuestion() {
     const question = this.fb.group({
       question: [],
-      img: []
-    })
+      img: [],
+      open: false,
+    });
 
-    this.questionForms.push(question)
+    this.questionForms.push(question);
   }
 
-  deleteQuestion(i){
-    this.questionForms.removeAt(i)
+  deleteQuestion(i) {
+    this.questionForms.removeAt(i);
   }
 
-  submitAnketa(){
-    this.anketyService.createAnketa(this.anketaForm.value).subscribe(data =>{
-      this.router.navigateByUrl('/admin/ankety/new')
-    })
+  submitAnketa() {
+    this.anketyService.createAnketa(this.anketaForm.value).subscribe((data) => {
+      this.router.navigateByUrl("/admin/ankety/new");
+    });
   }
-  
-
 }
--- src/app/components/play/play.component.ts
+++ src/app/components/play/play.component.ts
@@ -1,52 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import { PlayService } from 'src/app/services/play.service';
-import { ActivatedRoute } from '@angular/router';
-import { ResultsService } from 'src/app/services/results.service';
+import { Component, OnInit } from "@angular/core";
+import { PlayService } from "src/app/services/play.service";
+import { ActivatedRoute } from "@angular/router";
+import { ResultsService } from "src/app/services/results.service";
 
 @Component({
-  selector: 'app-play',
-  templateUrl: './play.component.html',
-  styleUrls: ['./play.component.css']
+  selector: "app-play",
+  templateUrl: "./play.component.html",
+  styleUrls: ["./play.component.css"],
 })
 export class PlayComponent implements OnInit {
-  anketa
-  stage = 0
+  anketa;
+  textarea_value = "";
+  stage = 0;
   result = {
     answers: [],
-    anketa_id:''
-  }
-  questionNumber: number = 0
-  constructor(private playService: PlayService, private route: ActivatedRoute, private resultsService: ResultsService) { }
+    anketa_id: "",
+  };
+  questionNumber: number = 0;
+  constructor(
+    private playService: PlayService,
+    private route: ActivatedRoute,
+    private resultsService: ResultsService
+  ) {}
 
   ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('id')
-    this.playService.getAneta(id).subscribe(data => {
-      this.anketa = data
-      this.result.anketa_id = this.anketa._id
-    })
+    const id = this.route.snapshot.paramMap.get("id");
+    this.playService.getAneta(id).subscribe((data) => {
+      this.anketa = data;
+      this.result.anketa_id = this.anketa._id;
+    });
   }
 
   nextStage() {
-    this.stage++
+    this.stage++;
   }
 
   answer(answer: number = 1) {
+    console.log(this.result);
+    this.textarea_value = "";
+    console.log(this.textarea_value);
+
     this.result.answers.push({
       question: this.anketa.questions[this.questionNumber].question,
-      answer
-    })
-    if (this.questionNumber == this.anketa.questions.length - 1){
-      this.nextStage()
-      this.postResult()
-    }else{
-      this.questionNumber++
+      answer,
+    });
+    if (this.questionNumber == this.anketa.questions.length - 1) {
+      this.nextStage();
+      this.postResult();
+    } else {
+      this.questionNumber++;
     }
   }
 
-  postResult(){
-    this.resultsService.postResults(this.result).subscribe(data =>{
-      console.log(data)
-    })
+  postResult() {
+    this.resultsService.postResults(this.result).subscribe((data) => {
+      console.log(data);
+    });
   }
-
 }
